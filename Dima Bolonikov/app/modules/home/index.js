e6a3e4594f2028b6f0735737d0b04ca30e1a27b5
Dima Bolonikov <bolonickov.dmitry@icloud.com>
2019-06-01 18:57:21 +0300

    add lab 3

--- Dima Bolonikov/app/modules/home/index.js
+++
@@ -1,7 +0,0 @@
-import * as actions from './actions';
-import * as constants from './constants';
-import reducer from './reducer';
-
-import * as theme from '../../config/theme';
-
-export { actions, constants, reducer, theme };
